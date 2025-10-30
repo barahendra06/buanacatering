ac65a134faa5c49a3f58fa9ac33495449f31a8a7
barahendra06 <barahendra06@gmail.com>
2025-10-30 23:20:03 +0700

    Updated naming owner

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,5 +5,5 @@
         
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2025 <a href="">leonantech.com</a>.</strong>
+    <strong>Copyright &copy; 2025 <a href="">barahendra06</a>.</strong>
 </footer>
